38ea4422227d5a3e5446cccf52ad00fe1300f637
horikawa.takahiro@gmail.com <horikawa.takahiro@gmail.com@cb0f6bdd-25b6-651e-ed8d-bbfca9c089ba>
2011-09-21 23:54:16 +0000

    initial commit

---
+++ trunk/android-face-demo/build/jni/CMakeFiles/TestBar.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "foobarJAVA_wrap.cxx"
+  "CMakeFiles/TestBar.dir/foobarJAVA_wrap.cxx.o"
+  "CMakeFiles/TestBar.dir/TestBar.cpp.o"
+  "foobarJAVA_wrap.cxx"
+  "../../libs/armeabi-v7a/libTestBar.pdb"
+  "../../libs/armeabi-v7a/libTestBar.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/TestBar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
