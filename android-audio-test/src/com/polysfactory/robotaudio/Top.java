c02e28ef8c00ffbaeb595d52113b670a80556d17
horikawa.takahiro <horikawa.takahiro@gmail.com>
2011-10-10 12:51:06 +0000

    add new project for test

---
+++ android-audio-test/src/com/polysfactory/robotaudio/Top.java
@@ -0,0 +1,15 @@
+package com.polysfactory.robotaudio;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Top extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
