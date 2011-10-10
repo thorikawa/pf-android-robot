ec1adbaa6b490ef96282aa4cc64c01df827309f3
horikawa.takahiro <horikawa.takahiro@gmail.com>
2011-10-10 15:50:13 +0000

    add logging

--- android-audio-test/jni/RobotAudio.cpp
+++ android-audio-test/jni/RobotAudio.cpp
@@ -1,19 +1,20 @@
 #include "RobotAudio.h"
-
+#include "log.h"
 
 void RobotAudio::openFiles(WavInFile **inFile, WavOutFile **outFile)
 {
-    int bits, samplerate, channels;
+  LOGD("openFiles");
+  int bits, samplerate, channels;
 
-    // open input file...
-    *inFile = new WavInFile("/sdcard/poly.wav");
+  // open input file...
+  *inFile = new WavInFile("/sdcard/poly.wav");
 
-    // ... open output file with same sound parameters
-    bits = (int)(*inFile)->getNumBits();
-    samplerate = (int)(*inFile)->getSampleRate();
-    channels = (int)(*inFile)->getNumChannels();
+  // ... open output file with same sound parameters
+  bits = (int)(*inFile)->getNumBits();
+  samplerate = (int)(*inFile)->getSampleRate();
+  channels = (int)(*inFile)->getNumChannels();
 
-    *outFile = new WavOutFile("/sdcard/robot.wav", samplerate, bits, channels);
+  *outFile = new WavOutFile("/sdcard/robot.wav", samplerate, bits, channels);
 }
 
 
@@ -22,29 +23,30 @@ void RobotAudio::openFiles(WavInFile **inFile, WavOutFile **outFile)
 // command line parameters
 void RobotAudio::setup(SoundTouch *pSoundTouch, const WavInFile *inFile)
 {
-    int sampleRate;
-    int channels;
+  LOGD("setup");
+  int sampleRate;
+  int channels;
 
-    sampleRate = (int)inFile->getSampleRate();
-    channels = (int)inFile->getNumChannels();
-    pSoundTouch->setSampleRate(sampleRate);
-    pSoundTouch->setChannels(channels);
-    //pSoundTouch->setTempoChange(params->tempoDelta);
-    pSoundTouch->setPitchSemiTones(20.0F);
-    //pSoundTouch->setRateChange(params->rateDelta);
-    //pSoundTouch->setSetting(SETTING_USE_QUICKSEEK, params->quick);
-    //pSoundTouch->setSetting(SETTING_USE_AA_FILTER, !(params->noAntiAlias));
+  sampleRate = (int)inFile->getSampleRate();
+  channels = (int)inFile->getNumChannels();
+  pSoundTouch->setSampleRate(sampleRate);
+  pSoundTouch->setChannels(channels);
+  //pSoundTouch->setTempoChange(params->tempoDelta);
+  pSoundTouch->setPitchSemiTones(20.0F);
+  //pSoundTouch->setRateChange(params->rateDelta);
+  //pSoundTouch->setSetting(SETTING_USE_QUICKSEEK, params->quick);
+  //pSoundTouch->setSetting(SETTING_USE_AA_FILTER, !(params->noAntiAlias));
 
-    /*
-    if (params->speech)
-    {
-        // use settings for speech processing
-        pSoundTouch->setSetting(SETTING_SEQUENCE_MS, 40);
-        pSoundTouch->setSetting(SETTING_SEEKWINDOW_MS, 15);
-        pSoundTouch->setSetting(SETTING_OVERLAP_MS, 8);
-        fprintf(stderr, "Tune processing parameters for speech processing.\n");
-    }
-    */
+  /*
+  if (params->speech)
+  {
+      // use settings for speech processing
+      pSoundTouch->setSetting(SETTING_SEQUENCE_MS, 40);
+      pSoundTouch->setSetting(SETTING_SEEKWINDOW_MS, 15);
+      pSoundTouch->setSetting(SETTING_OVERLAP_MS, 8);
+      fprintf(stderr, "Tune processing parameters for speech processing.\n");
+  }
+  */
 }
 
 
@@ -53,83 +55,85 @@ void RobotAudio::setup(SoundTouch *pSoundTouch, const WavInFile *inFile)
 // Processes the sound
 void RobotAudio::process(SoundTouch *pSoundTouch, WavInFile *inFile, WavOutFile *outFile)
 {
-    int nSamples;
-    int nChannels;
-    int buffSizeSamples;
-    SAMPLETYPE sampleBuffer[BUFF_SIZE];
+  LOGD("process");
+  int nSamples;
+  int nChannels;
+  int buffSizeSamples;
+  SAMPLETYPE sampleBuffer[BUFF_SIZE];
 
-    if ((inFile == NULL) || (outFile == NULL)) return;  // nothing to do.
+  if ((inFile == NULL) || (outFile == NULL)) return;  // nothing to do.
 
-    nChannels = (int)inFile->getNumChannels();
-    assert(nChannels > 0);
-    buffSizeSamples = BUFF_SIZE / nChannels;
+  nChannels = (int)inFile->getNumChannels();
+  assert(nChannels > 0);
+  buffSizeSamples = BUFF_SIZE / nChannels;
 
-    // Process samples read from the input file
-    while (inFile->eof() == 0)
-    {
-        int num;
+  // Process samples read from the input file
+  while (inFile->eof() == 0)
+  {
+      int num;
 
-        // Read a chunk of samples from the input file
-        num = inFile->read(sampleBuffer, BUFF_SIZE);
-        nSamples = num / (int)inFile->getNumChannels();
+      // Read a chunk of samples from the input file
+      num = inFile->read(sampleBuffer, BUFF_SIZE);
+      nSamples = num / (int)inFile->getNumChannels();
 
-        // Feed the samples into SoundTouch processor
-        pSoundTouch->putSamples(sampleBuffer, nSamples);
+      // Feed the samples into SoundTouch processor
+      pSoundTouch->putSamples(sampleBuffer, nSamples);
 
-        // Read ready samples from SoundTouch processor & write them output file.
-        // NOTES:
-        // - 'receiveSamples' doesn't necessarily return any samples at all
-        //   during some rounds!
-        // - On the other hand, during some round 'receiveSamples' may have more
-        //   ready samples than would fit into 'sampleBuffer', and for this reason 
-        //   the 'receiveSamples' call is iterated for as many times as it
-        //   outputs samples.
-        do 
-        {
-            nSamples = pSoundTouch->receiveSamples(sampleBuffer, buffSizeSamples);
-            outFile->write(sampleBuffer, nSamples * nChannels);
-        } while (nSamples != 0);
-    }
+      // Read ready samples from SoundTouch processor & write them output file.
+      // NOTES:
+      // - 'receiveSamples' doesn't necessarily return any samples at all
+      //   during some rounds!
+      // - On the other hand, during some round 'receiveSamples' may have more
+      //   ready samples than would fit into 'sampleBuffer', and for this reason 
+      //   the 'receiveSamples' call is iterated for as many times as it
+      //   outputs samples.
+      do 
+      {
+          nSamples = pSoundTouch->receiveSamples(sampleBuffer, buffSizeSamples);
+          outFile->write(sampleBuffer, nSamples * nChannels);
+      } while (nSamples != 0);
+  }
 
-    // Now the input file is processed, yet 'flush' few last samples that are
-    // hiding in the SoundTouch's internal processing pipeline.
-    pSoundTouch->flush();
-    do 
-    {
-        nSamples = pSoundTouch->receiveSamples(sampleBuffer, buffSizeSamples);
-        outFile->write(sampleBuffer, nSamples * nChannels);
-    } while (nSamples != 0);
+  // Now the input file is processed, yet 'flush' few last samples that are
+  // hiding in the SoundTouch's internal processing pipeline.
+  pSoundTouch->flush();
+  do 
+  {
+      nSamples = pSoundTouch->receiveSamples(sampleBuffer, buffSizeSamples);
+      outFile->write(sampleBuffer, nSamples * nChannels);
+  } while (nSamples != 0);
 }
 
 int RobotAudio::execute()
 {
-    WavInFile *inFile;
-    WavOutFile *outFile;
-    SoundTouch soundTouch;
+  LOGD("execute start");
+  WavInFile *inFile;
+  WavOutFile *outFile;
+  SoundTouch soundTouch;
 
-    try 
-    {
-        // Open input & output files
-        openFiles(&inFile, &outFile);
+  try 
+  {
+      // Open input & output files
+      openFiles(&inFile, &outFile);
 
-        // Setup the 'SoundTouch' object for processing the sound
-        setup(&soundTouch, inFile);
+      // Setup the 'SoundTouch' object for processing the sound
+      setup(&soundTouch, inFile);
 
-        // Process the sound
-        process(&soundTouch, inFile, outFile);
+      // Process the sound
+      process(&soundTouch, inFile, outFile);
 
-        // Close WAV file handles & dispose of the objects
-        delete inFile;
-        delete outFile;
+      // Close WAV file handles & dispose of the objects
+      delete inFile;
+      delete outFile;
 
-        fprintf(stderr, "Done!\n");
-    } 
-    catch (const runtime_error &e) 
-    {
-        // An exception occurred during processing, display an error message
-        fprintf(stderr, "%s\n", e.what());
-        return -1;
-    }
-
-    return 0;
+      fprintf(stderr, "Done!\n");
+  } 
+  catch (const runtime_error &e) 
+  {
+      // An exception occurred during processing, display an error message
+      fprintf(stderr, "%s\n", e.what());
+      return -1;
+  }
+  LOGD("execute end");
+  return 0;
 }
