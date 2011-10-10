2727cf6b879755d73cc15b445bdbf43b96b4e8dc
horikawa.takahiro@gmail.com <horikawa.takahiro@gmail.com@cb0f6bdd-25b6-651e-ed8d-bbfca9c089ba>
2011-10-10 15:53:48 +0000

    add audio convert block

--- trunk/android-audio-test/src/com/polysfactory/robotaudio/Top.java
+++ trunk/android-audio-test/src/com/polysfactory/robotaudio/Top.java
@@ -3,13 +3,15 @@ package com.polysfactory.robotaudio;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class Top extends Activity
-{
+import com.polysfactory.robotaudio.jni.RobotAudio;
+
+public class Top extends Activity {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        RobotAudio robotAudio = new RobotAudio();
+        robotAudio.execute();
     }
 }
