3187b236c5c40abfb4d43e05b900f4789b0e9ede
horikawa.takahiro <horikawa.takahiro@gmail.com>
2011-10-15 07:28:39 +0000

    refactoring

--- android/src/com/google/android/DemoKit/Top.java
+++ android/src/com/google/android/DemoKit/Top.java
@@ -1,19 +1,8 @@
 package com.google.android.DemoKit;
 
-import java.io.FileDescriptor;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 import android.app.Activity;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.os.ParcelFileDescriptor;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -26,35 +15,23 @@ public class Top extends Activity implements Runnable {
 
     private static final String ACTION_USB_PERMISSION = "com.google.android.DemoKit.action.USB_PERMISSION";
 
-    private UsbManager mUsbManager;
-
-    private PendingIntent mPermissionIntent;
-
-    private boolean mPermissionRequestPending;
-
     UsbAccessory mAccessory;
 
-    ParcelFileDescriptor mFileDescriptor;
-
-    FileInputStream mInputStream;
-
-    FileOutputStream mOutputStream;
-
     int servo1Level = 0;
+
+    UsbCommander usbCommander;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mUsbManager = UsbManager.getInstance(this);
-        mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+        usbCommander = new UsbCommander(this);
         IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
         filter.addAction(UsbManager.ACTION_USB_ACCESSORY_DETACHED);
-        registerReceiver(mUsbReceiver, filter);
 
         if (getLastNonConfigurationInstance() != null) {
             mAccessory = (UsbAccessory) getLastNonConfigurationInstance();
-            openAccessory(mAccessory);
+            usbCommander.openAccessory(mAccessory);
         }
 
         setContentView(R.layout.top);
@@ -63,143 +40,37 @@ public class Top extends Activity implements Runnable {
         Button backwardMotor1Button = (Button) findViewById(R.id.BackwardMotor1);
         stopMotor1Button.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
-                // MotorCommand mc = new MotorCommand();
-                // mc.setCommandByte(MotorCommand.motor2Stop);
-                ServoCommand sc = new ServoCommand();
-                servo1Level += 15;
-                if (servo1Level >= 64) {
-                    servo1Level = 0;
-                }
-                sc.setDegree(servo1Level);
-                try {
-                    // mOutputStream.write(mc.toBytes());
-                    mOutputStream.write(sc.toBytes());
-                } catch (IOException e) {
-                    // TODO 自動生成された catch ブロック
-                    e.printStackTrace();
-                }
+                usbCommander.forward();
             }
         });
         forwardMotor1Button.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
-                // MotorCommand mc = new MotorCommand();
-                // mc.setCommandByte(MotorCommand.motor2Forward);
-                ServoCommand sc = new ServoCommand();
-                servo1Level -= 15;
-                if (servo1Level < 0) {
-                    servo1Level = 63;
-                }
-                sc.setDegree(servo1Level);
-                try {
-                    mOutputStream.write(sc.toBytes());
-                } catch (IOException e) {
-                    // TODO 自動生成された catch ブロック
-                    e.printStackTrace();
-                }
+                usbCommander.backward();
             }
         });
         backwardMotor1Button.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
-                MotorCommand mc = new MotorCommand();
-                mc.setCommandByte(MotorCommand.motor2Backward);
-                try {
-                    mOutputStream.write(mc.toBytes());
-                } catch (IOException e) {
-                    // TODO 自動生成された catch ブロック
-                    e.printStackTrace();
-                }
+                usbCommander.spinTurnLeft();
             }
         });
     }
 
     @Override
     protected void onDestroy() {
-        unregisterReceiver(mUsbReceiver);
+        usbCommander.unregisterReceiver();
         super.onDestroy();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        closeAccessory();
+        usbCommander.closeAccessory();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (mInputStream != null && mOutputStream != null) {
-            return;
-        }
-
-        UsbAccessory[] accessories = mUsbManager.getAccessoryList();
-        UsbAccessory accessory = (accessories == null ? null : accessories[0]);
-        if (accessory != null) {
-            if (mUsbManager.hasPermission(accessory)) {
-                openAccessory(accessory);
-            } else {
-                synchronized (mUsbReceiver) {
-                    if (!mPermissionRequestPending) {
-                        mUsbManager.requestPermission(accessory, mPermissionIntent);
-                        mPermissionRequestPending = true;
-                    }
-                }
-            }
-        } else {
-            Log.d(TAG, "mAccessory is null");
-        }
-    }
-
-    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (ACTION_USB_PERMISSION.equals(action)) {
-                synchronized (this) {
-                    UsbAccessory accessory = UsbManager.getAccessory(intent);
-                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
-                        openAccessory(accessory);
-                    } else {
-                        Log.d(TAG, "permission denied for accessory " + accessory);
-                    }
-                    mPermissionRequestPending = false;
-                }
-            } else if (UsbManager.ACTION_USB_ACCESSORY_DETACHED.equals(action)) {
-                UsbAccessory accessory = UsbManager.getAccessory(intent);
-                if (accessory != null && accessory.equals(mAccessory)) {
-                    closeAccessory();
-                }
-            }
-        }
-    };
-
-    private void openAccessory(UsbAccessory accessory) {
-        mFileDescriptor = mUsbManager.openAccessory(accessory);
-        if (mFileDescriptor != null) {
-            mAccessory = accessory;
-            FileDescriptor fd = mFileDescriptor.getFileDescriptor();
-            mInputStream = new FileInputStream(fd);
-            mOutputStream = new FileOutputStream(fd);
-            Thread thread = new Thread(null, this, "DemoKit");
-            thread.start();
-            Log.d(TAG, "accessory opened");
-            enableControls(true);
-        } else {
-            Log.d(TAG, "accessory open fail");
-        }
-    }
-
-    private void closeAccessory() {
-        enableControls(false);
-
-        try {
-            if (mFileDescriptor != null) {
-                mFileDescriptor.close();
-            }
-        } catch (IOException e) {
-        } finally {
-            mFileDescriptor = null;
-            mAccessory = null;
-        }
+        usbCommander.reopen();
     }
 
     protected void enableControls(boolean enable) {
