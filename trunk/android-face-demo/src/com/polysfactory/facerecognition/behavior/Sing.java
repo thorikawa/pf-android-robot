625b8461db74be4996d763dcc72541867c2982f9
horikawa.takahiro@gmail.com <horikawa.takahiro@gmail.com@cb0f6bdd-25b6-651e-ed8d-bbfca9c089ba>
2011-11-01 00:24:00 +0000

    modify behaviors

--- trunk/android-face-demo/src/com/polysfactory/facerecognition/behavior/Sing.java
+++ trunk/android-face-demo/src/com/polysfactory/facerecognition/behavior/Sing.java
@@ -4,6 +4,7 @@ import android.net.Uri;
 import android.util.Log;
 
 import com.polysfactory.facerecognition.App;
+import com.polysfactory.facerecognition.CommandUtils;
 import com.polysfactory.facerecognition.R;
 
 public class Sing extends Behavior {
@@ -18,11 +19,29 @@ public class Sing extends Behavior {
             mAudioCommander.playMusic(
                     Uri.parse("android.resource://com.polysfactory.facerecognition/" + R.raw.droidohatsuzuku)
                     );
+            for (int i = 0; i < 4; i++) {
+                CommandUtils.randomEye(mUsbCommander);
+                sleep(100);
+                mUsbCommander.forward();
+                sleep(1000);
+                mUsbCommander.spinTurnRight();
+                sleep(500);
+            }
+            mUsbCommander.stop();
+            sleep(100);
+            mUsbCommander.lightLed(0);
+            sleep(100);
             break;
         case 1:
             mAudioCommander.playMusic(
                     Uri.parse("android.resource://com.polysfactory.facerecognition/" + R.raw.polys_light_normal)
                     );
+            for (int i = 0; i < 13; i++) {
+                CommandUtils.randomEye(mUsbCommander);
+                sleep(700);
+            }
+            mUsbCommander.lightLed(0);
+            sleep(100);
             break;
         default:
             break;
